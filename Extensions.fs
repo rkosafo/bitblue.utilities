370cac648df1c883dbb88ce51bffc5bd851ae205
RKO <rkosafo@outlook.com>
2019-05-18 08:26:05 +0000

    Added project files

---
+++ Extensions.fs
@@ -0,0 +1,37 @@
+﻿namespace App.Utils
+
+
+open System
+
+[<AutoOpen>]
+module Extensions =
+  type String with
+    ///Returns ifNull is value isNullOrEmpty else value
+    static member orElse ifNull value =
+      if String.IsNullOrEmpty value then ifNull
+      else value
+
+  type Exception with
+    member x.innerMost =
+      let rec f (x: exn) = match isNull x.InnerException with false -> f x.InnerException | _ -> x
+      f x
+
+  module List =
+    let safeSkip n xs =
+      xs
+      |> List.indexed //.mapi (fun i x -> i, x)
+      |> List.choose (fun (i, x) -> if i >= n then Some x else None)
+    
+    ///Can this be replaced with List.truncate?
+    let safeTake n xs =
+      xs
+      |> List.indexed //mapi (fun i x -> i, x)
+      |> List.choose (fun (i, x) -> if i < n then Some x else None)
+
+    let rec intersperse sep = function
+      | [] -> []
+      | [x] -> [x]
+      | x :: xs -> x :: sep :: intersperse sep xs
+
+  module Seq =
+    let all f xs = not <| Seq.exists (not << f) xs
---
+++ GeneralUtilities.fs
@@ -0,0 +1,34 @@
+﻿namespace App.Utils
+
+
+open System
+
+
+[<AutoOpen>]
+module GeneralUtilities =
+  let expand template (data:Map<string, string>) =
+    let pattern = "\\${(.*?)}"
+    let f (m:Regex.RegexMatch) =
+      let key = m.Groups.[1].Value
+      match Map.tryFind key data with
+      | Some x -> x
+      | _ -> m.Groups.[0].Value
+    Regex.replaceWithFunction pattern f template
+
+  ///Expands the template multiple times. This is to handle instances where the data values are templates
+  let nExpand n template data =
+    let rec loop (template:string) data n =
+      if n=0 || not (template.Contains "${") then template
+      else loop (expand template data) data (n-1)
+    loop template data n
+
+  let (|Contains|_|) subString x =
+    if String.contains subString x then Some ()
+    else None
+
+  let limitText cnt text =
+    match String.length text with
+    | x when x <= cnt -> text
+    | x when cnt > 3 -> text.[0 .. cnt - 3 - 1] + "..."
+    | _ -> text.[0 .. cnt - 1]
+
